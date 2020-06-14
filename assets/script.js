8f3192a347e41466e70e120344e6c396e8db9569
Cherie Cookson <cheriecookson@gmail.com>
2020-06-14 11:59:18 -0500

    added more styling that doesnt work.  Working on getting quiz to function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 var startBtn=document.getElementById("startBtn");
- var startSection=document.getElementById("start");
+var startSection=document.getElementById("start");
 var answerBtn1=document.getElementById("answerBtn1");
 var answerBtn2=document.getElementById("answerBtn2");
 var answerBtn3=document.getElementById("answerBtn3");
 var answerBtn4=document.getElementById("answerBtn4");
 var quizSection=document.getElementById("quiz");
- // var question=document.getElementById("question");
+var question=document.getElementById("question");
+var confirm=document.getElementById("confirm");
 var submitBtn=document.getElementById("submitBtn");
 var finalScoreSection=document.getElementById("finalScore");
 var goBackBtn=document.getElementById("goBackBtn");
@@ -16,23 +17,53 @@ var emailInput = document.querySelector("#email");
 var userEmailSpan = document.querySelector("#user-email");
 
 
+    quiz = [
+    {
+        question : "Ask a good qestion for question one?",
+        answerBtn1 : "Answer 1",
+        answerBtn2 : "Answer 2",
+        answerBtn3 : "Answer 3",
+        answerBtn3 : "Answer 4",
+    },{
+        question : "Ask a really good qestion for question two?",
+        answerBtn1 : "Answer 1",
+        answerBtn2 : "Answer 2",
+        answerBtn3 : "Answer 3",
+        answerBtn3 : "Answer 4",
+    },{
+        question : "Ask a really really good qestion for question three?",
+        answerBtn1 : "Answer 1",
+        answerBtn2 : "Answer 2",
+        answerBtn3 : "Answer 3",
+        answerBtn3 : "Answer 4",
+    }
+    ];
 
-// var questions = [
-//     {
-//         question : "Ask a good qestion for question one?",
-//         multiChoiceA : "Answer 1",
-//         multiChoiceB : "Answer 2",
-//     },{
-//         question : "Ask a really good qestion for question two?",
-//         multiChoiceA : "Answer 1",
-//         multiChoiceB : "Answer 2",
-//     },{
-//         question : "Ask a really really good qestion for question three?",
-//         multiChoiceA : "Answer 1",
-//         multiChoiceB : "Answer 2",
-//     }
-// ];
 
+// ********** Trying to figure this part out ***********
+    //   // We start the game with a score of 0.
+    //   var score = 0;
+
+    //   // Loop over every question object
+    //   for (var i = 0; i < quiz.length; i++) {
+    //     // Display current question to user and ask OK/Cancel
+    //     var answer = confirm(quiz[i].question);
+
+    //     // Compare answers
+    //     if (
+    //       (answer === true && quiz[i].a === 't') ||
+    //       (answer === false && quiz[i].a === 'f')
+    //     ) {
+    //       // Increase score
+    //       score++;
+    //       alert('Correct!');
+    //     } else {
+    //       alert('Wrong!');
+    //     }
+    //   }
+// *******************************************************************
+
+// Local storage function
 function renderLastRegistered() {
     var email = localStorage.getItem("email");
     if (email === null) {
@@ -41,13 +72,15 @@ function renderLastRegistered() {
 
   } renderLastRegistered();
 
-  
   submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
     var email = document.querySelector("#email").value;
     localStorage.setItem("email", email);
       renderLastRegistered();
+// Clear storage not working ****************************
+      localStorage.clear("email", email);
   });
+
 
 
 function startButtonHandler () {
@@ -58,7 +91,6 @@ function startButtonHandler () {
 function answerButtonHandler () {
     quizSection.className="hide";
     finalScoreSection.classList.remove("hide");
-    console.log(answerBtn2);
 }
 
 function submitButtonHandler () {
@@ -67,15 +99,15 @@ function submitButtonHandler () {
 }
 
 function goBackButtonHandler () {
+    finalScoreSection.classList.remove("hide");
     highScoreSection.className="hide";
-    startSection.classList.remove("hide");
 }
 
 function clearButtonHandler () {
     highScoreSection.className="hide";
     startSection.classList.remove("hide");
+    
 }
-
 
 
 
